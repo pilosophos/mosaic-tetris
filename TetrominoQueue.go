cc85c1a3f13b43a8c57aeb7215b2580ad9f9ba09
pilosophos <48043580+pilosophos@users.noreply.github.com>
2023-06-10 20:35:55 -0700

    update comment

--- TetrominoQueue.go
+++ TetrominoQueue.go
@@ -26,10 +26,10 @@ func (tq *TetrominoQueue) RefreshQueue() {
 
 	tetrominoShapes := []*UnplacedTetromino{
 		NewUnplacedTetromino([][2]int{{0, 0}, {1, 0}, {2, 0}, {3, 0}}, startLoc, startTime, "\033[1;36m"), // I (cyan)
-		NewUnplacedTetromino([][2]int{{0, 0}, {1, 0}, {0, 1}, {1, 1}}, startLoc, startTime, "\033[1;37m"), // O (yellow)
+		NewUnplacedTetromino([][2]int{{0, 0}, {1, 0}, {0, 1}, {1, 1}}, startLoc, startTime, "\033[1;37m"), // O (white)
 		NewUnplacedTetromino([][2]int{{0, 0}, {1, 0}, {2, 0}, {1, 1}}, startLoc, startTime, "\033[1;35m"), // T (magenta)
 		NewUnplacedTetromino([][2]int{{1, 0}, {1, 1}, {1, 2}, {0, 2}}, startLoc, startTime, "\033[1;34m"), // J (blue)
-		NewUnplacedTetromino([][2]int{{0, 0}, {0, 1}, {1, 2}, {0, 2}}, startLoc, startTime, "\033[1;33m"), // L (orange)
+		NewUnplacedTetromino([][2]int{{0, 0}, {0, 1}, {1, 2}, {0, 2}}, startLoc, startTime, "\033[1;33m"), // L (yellow)
 		NewUnplacedTetromino([][2]int{{0, 1}, {1, 1}, {1, 0}, {2, 0}}, startLoc, startTime, "\033[1;32m"), // S (green)
 		NewUnplacedTetromino([][2]int{{0, 0}, {1, 0}, {1, 1}, {2, 1}}, startLoc, startTime, "\033[1;31m"), // Z (red)
 	}
