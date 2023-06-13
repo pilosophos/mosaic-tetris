d3ca63d216b858d58a0adb24cecdad4ecb5243f7
pilosophos <48043580+pilosophos@users.noreply.github.com>
2023-06-13 15:27:50 -0700

    fix typos in build names

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 GOOS=darwin GOARCH=amd64 go build -o ./build/mosaic-tetris-mac .
-GOOS=windows GOARCH=amd64 go build -o ./build/mosaic-tetris-windows . 
-go build -o ./build/mosaic-tatris-linux .
+GOOS=windows GOARCH=amd64 go build -o ./build/mosaic-tetris-windows.exe . 
+go build -o ./build/mosaic-tetris-linux .
